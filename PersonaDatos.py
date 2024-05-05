fbc696c1e0667760678cb1e7838dfa79d5386ccd
Alexander Apraez <apraez.consultant@gmail.com>
2024-05-05 00:52:27 -0500

    versión con  inicializar cursor y db antes del bloque try/except, asignándoles un valor de None luego  bloque finally, verificar si cursor y db no son None antes de intentar cerrarlo

--- PersonaDatos.py
+++ PersonaDatos.py
@@ -4,7 +4,7 @@ def save(persona):
     persona= dict(persona)
     cursor = None
     db = None
-    
+
     try: 
         db=con.conectar()
         cursor=db.cursor()
@@ -18,12 +18,10 @@ def save(persona):
         creada= cursor.rowcount>0
         db.commit()
         if creada:
-            cursor.close()
-            db.close()
+            
             return {"respuesta": creada, "mensaje":"Persona registrada"}
         else:
-            cursor.close()
-            db.close()
+            
             return  {"respuesta": creada, "mensaje":"No se logró registrar a la persona"}
     except Exception as ex:
         if "UNIQUE" in str(ex) and "Email" in str(ex):
@@ -32,8 +30,7 @@ def save(persona):
             mensaje= "Ya existe una persona con ese DNI"
         else: 
             mensaje = str(ex)
-        cursor.close()
-        db.close()
+        
         return {"respuesta": False, "mensaje":mensaje}
     
 
@@ -44,15 +41,18 @@ def findALL():
         cursor.execute("SELECT * FROM personas")
         personas = cursor.fetchall()
         if personas: 
-            cursor.close()
-            db.close()
+            
             return{"respuesta":True, "personas": personas, "mensaje":"Listado ok"}
         else:
-            cursor.close()
-            db.close()
+            
             return{"respuesta":False, "mensaje":"NO hay personas registradas aún"}
     except Exception as ex:
-        cursor.close()
-        db.close()
+        
         return {"respuesta": False, "mensaje":str(ex)}
+    
+    finally:
+        if cursor is not None:
+            cursor.close()
+        if db is not None:
+            db.close()
     
